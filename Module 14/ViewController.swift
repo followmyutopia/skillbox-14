955616831bd66a1ffae2f8b4f6dd2387acb4a338
Avicus Delacroix <followmyutopia@gmail.com>
2022-06-01 00:28:51 +0300

    Initial Commit

---
+++ Module 14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module 14
+//
+//  Created by Avicus Delacroix on 01.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
